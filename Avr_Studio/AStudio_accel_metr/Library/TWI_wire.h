e84f8493846441c03a3b9612b008dd4919ae516a
AntonNikitin <kb.nikitinav@mail.ru>
2019-01-15 15:36:34 +0300

    побежден i2c
    -библиотека с функцией записи и функцией чтения
    -в main успешный запрос регистра WhoAmI

--- Avr_Studio/AStudio_accel_metr/Library/TWI_wire.h
+++ Avr_Studio/AStudio_accel_metr/Library/TWI_wire.h
@@ -7,12 +7,10 @@
 #include <util/twi.h>
 
 void TWI_init_speed(uint8_t prescal)	{
-	TWSR = 0;	// TWPS = 1;
-	
 	TWBR = prescal;
 	
 	// SCLfreq = CPU/(16+ 2*prescal*4^TWPS )
-	// TWPS = 1;
+	// TWPS = 0;
 	// prescal = 18 -> SCLfreq = 100 kHz
 	// prescal = 250 -> SCLfreq = 7,9 kHz
 }
@@ -23,43 +21,25 @@ void TWI_start_write_stop(uint8_t addres, uint8_t DATA)	{
 	TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
 	      //запуск TWI / TWI мастер / TWI включен
 	while( !(TWCR & (1<<TWINT)) );
+	
 	// проверка, что TWINT сброшен, т.е. операция выполнена
 	/*
-	тут должна быть проверка регистров статуса, вместо него пока просто задержка
+	тут должна быть проверка регистров статуса
 	*/
-			UART_write( TWSR );
-			delay_counters(300000);
-			UART_write( 0xC1 );
-			delay_counters(300000);
-	
-	
-	TWDR = addres;
-			
+	TWDR = (addres<<1)|(0<<0);
 	TWCR = (1<<TWINT) | (1<<TWEN);
 		  //запуск TWI
 	while( !(TWCR & (1<<TWINT)) );
 	// проверка, что TWINT выставлен, т.е. операция выполнена
 	/*
-	тут тоже должна быть проверка регистров статуса, вместо него тоже пока просто задержка
+	тут тоже должна быть проверка регистров статуса
 	*/
-			UART_write( TWSR );
-			delay_counters(300000);
-			UART_write( 0xC2 );
-			delay_counters(300000);
-	/*
 	TWDR = DATA;
-	
 	TWCR = (1<<TWINT) | (1<<TWEN);
 	while( !(TWCR & (1<<TWINT)) );
-	*/
 	/*
-	тут тоже должна быть проверка регистров статуса, вместо него тоже пока просто задержка
+	тут тоже должна быть проверка регистров статуса
 	*/
-			UART_write( TWSR );
-			delay_counters(300000);
-			UART_write( 0xC3 );
-			delay_counters(300000);
-			
 	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
 	
 }
@@ -68,31 +48,22 @@ uint8_t TWI_start_read_stop(uint8_t addres)	{
 	uint8_t DATA = 0xFF;
 	
 	TWCR = 0;
-	TWCR =  (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
-	      //запуск TWI / TWI мастер / TWI включен
+	TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
+	//запуск TWI / TWI мастер / TWI включен
 	while( !(TWCR & (1<<TWINT)) );
-	// проверка, что TWINT сброшен, т.е. операция выполнена
-	/*
-	тут должна быть проверка регистров статуса, вместо него пока просто задержка
-	*/
-			delay_counters(300000);
-	
-	TWDR = addres;
-	
+	// старт при чтении
+	TWDR = (addres<<1)|(1<<0);
 	TWCR = (1<<TWINT) | (1<<TWEN);
-		  //запуск TWI
+
 	while( !(TWCR & (1<<TWINT)) );
 	// проверка, что TWINT выставлен, т.е. операция выполнена
-	
-	/*
-	тут тоже должна быть проверка регистров статуса, вместо него тоже пока просто задержка
-	*/
-			delay_counters(300000);
-		
 	TWCR = (1<<TWINT) | (1<<TWEN);
-	DATA = TWDR;	
+	while( !(TWCR & (1<<TWINT)) );
 	
+	DATA = TWDR;
+
 	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
 	
 	return DATA;
 }
+
--- Avr_Studio/AStudio_accel_metr/main.c
+++ Avr_Studio/AStudio_accel_metr/main.c
@@ -21,7 +21,7 @@
 #define LIS3DH_REG_OUTADC3_L 0x0C
 #define LIS3DH_REG_WHOAMI    0x0F
 
-int8_t _i2caddr = 0x1A;//0x19;
+int8_t _i2caddr = 0x19;
 int8_t who_I_am = 0xAA;
 
 int main(void)
@@ -34,12 +34,12 @@ int main(void)
 	
 	digitalWrite_portB(0, HIGH);
 	
-	//TWI_init_speed(250);	// 18 -> 100kHz; 250 -> 7,9 kHz
+	TWI_init_speed(95);	// 18 -> 100kHz; 250 -> 7,9 kHz
 	
 	UART_write( 0xA0 );
 	UART_write( 0xA1 );
 	
-	i2c_programm_init(); 
+	//i2c_programm_init(); 
 	
 	UART_write( 0xA2 );
 	UART_write( 0xA3 );
@@ -52,42 +52,11 @@ int main(void)
 		digitalWrite_portB(0, HIGH);
 		delay_counters(50000);
 		
-		UART_write( 0x0A );
-		
-		// программный i2c	//
-		/*
-		i2c_start_cond();   // старт шины
-		i2c_send_byte (_i2caddr);   // адрес устройства, которое висит на шине
-		i2c_send_byte (LIS3DH_REG_WHOAMI);   // байт данных, который записываем в устройство
-		i2c_stop_cond();   // стоп шины
-		*/
-		i2c_restart_cond();
-		i2c_send_byte (_i2caddr);  // адрес записи
-		i2c_send_byte (LIS3DH_REG_WHOAMI);		// адрес секунд и десятков секунд (при дальнейшем чтении, адрес инкрементируется автоматически)
-		
-		who_I_am = i2c_get_byte(1);
-		
-		i2c_stop_cond();
-	/*
-		UART_write( 0x00 );
-		delay_counters(300000);
-		UART_write( _i2caddr );
-		delay_counters(300000);
-		UART_write( LIS3DH_REG_WHOAMI );
-		delay_counters(300000);
-		UART_write( 0xC0 );
-		delay_counters(300000);
-		
 		TWI_start_write_stop(_i2caddr, LIS3DH_REG_WHOAMI);
-		//i2c_start(_i2caddr);
-		
-		UART_write( 0x01 );
-		delay_counters(300000);
-		
-		//who_I_am = TWI_start_read_stop(_i2caddr);
-		//UART_write( 0x02 );
-		//delay_counters(300000);
-	*/	
+
+		delay_counters(200);
+
+		who_I_am = TWI_start_read_stop(_i2caddr);
 		
 		UART_write( who_I_am );
 	
