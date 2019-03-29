238e8a6e3a431229a043fae7cdcbafedee9692c6
AntonNikitin <kb.nikitinav@mail.ru>
2019-03-29 17:26:42 +0300

    задифайнен проект ардуины

--- Self_Arduno/sketch_mar28a/sketch_mar28a.ino
+++ Self_Arduno/sketch_mar28a/sketch_mar28a.ino
@@ -1,13 +1,34 @@
+#define PD0 0
+#define PD1 1
+#define PD2 2
+#define PD3 3
+#define PD4 4
+#define PD5 5
+#define PD6 6
+#define PD7 7
+#define PB0 8
+#define PB1 9
+#define PB2 10
+#define PB3 11
+#define PB4 12
+#define PB5 13
+
 void setup() {
-  // put your setup code here, to run once:
-  //DDRB |= (1<<PB1);
-  pinMode(9, OUTPUT);
+
+  pinMode(PB1, OUTPUT);
+  pinMode(PD3, OUTPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  PORTB |= (1<<1);
+  digitalWrite(PB1, HIGH);
   delay(50);
-  PORTB &= ~(1<<1);
+  digitalWrite(PB1, LOW);
+  delay(2000);
+
+  digitalWrite(PD3, HIGH);
+  delay(500);
+  digitalWrite(PD3, LOW);
   delay(1000);
 }
